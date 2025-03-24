e94321a505509a5a14f6881dcc5f72bb2d6fdd93
luiabdiel <luiabdieldev@gmail.com>
2025-03-24 20:57:42 -0300

    init: first commit

---
+++ src/main/java/br/com/luiabdiel/smart_health_system/SmartHealthSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.luiabdiel.smart_health_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHealthSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luiabdiel/smart_health_system/SmartHealthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luiabdiel.smart_health_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHealthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
