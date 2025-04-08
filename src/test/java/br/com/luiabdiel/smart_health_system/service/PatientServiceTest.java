461267fd6e5cac18b6fce92b29903ae924eb3124
luiabdiel <luiabdieldev@gmail.com>
2025-04-08 09:31:15 -0300

    test: find all patient

--- src/test/java/br/com/luiabdiel/smart_health_system/service/PatientServiceTest.java
+++ src/test/java/br/com/luiabdiel/smart_health_system/service/PatientServiceTest.java
@@ -8,10 +8,16 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -54,4 +60,29 @@ class PatientServiceTest {
         verify(this.patientRepository, times(1)).save(any());
     }
 
+    @Test
+    void shouldFindAllPatientSuccessfully() {
+        var expectedId = 1L;
+        var expectedName = "any";
+        var expectedCpf = "any";
+        var expectedBirthDate = LocalDate.of(2025,4, 8);
+        var expectedPhone = "any";
+
+        Patient patient = new Patient(
+                expectedId,
+                expectedName,
+                expectedCpf,
+                expectedBirthDate,
+                expectedPhone
+        );
+        Page<Patient> expectedPage = new PageImpl<>(List.of(patient));
+        Pageable pageable = PageRequest.of(0, 10);
+
+        when(this.patientRepository.findAll(pageable)).thenReturn(expectedPage);
+        Page<PatientResponseDto> response = this.patientService.findAll(pageable);
+
+        assertNotNull(response);
+        assertEquals(expectedId, response.getContent().get(0).getId());
+        verify(this.patientRepository, times(1)).findAll(pageable);
+    }
 }
