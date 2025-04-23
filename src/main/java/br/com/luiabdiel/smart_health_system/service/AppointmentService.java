151d951ad899d7d8358815c2145f1a851d6b5eed
luiabdiel <luiabdieldev@gmail.com>
2025-04-22 21:12:24 -0300

    feat: add logs

--- src/main/java/br/com/luiabdiel/smart_health_system/service/AppointmentService.java
+++ src/main/java/br/com/luiabdiel/smart_health_system/service/AppointmentService.java
@@ -9,12 +9,14 @@ import br.com.luiabdiel.smart_health_system.repository.AppointmentRepository;
 import br.com.luiabdiel.smart_health_system.repository.DoctorRepository;
 import br.com.luiabdiel.smart_health_system.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AppointmentService {
 
     private final AppointmentRepository appointmentRepository;
@@ -22,12 +24,21 @@ public class AppointmentService {
     private final DoctorRepository doctorRepository;
 
     public AppointmentResponseDto create(AppointmentRequestDto appointmentRequestDto) {
+        log.info("Iniciando criação de agendamento. Dados recebidos: {}", appointmentRequestDto);
+
         Patient patient = this.patientRepository
                 .findById(appointmentRequestDto.getPatientId())
-                .orElseThrow(() -> new IllegalArgumentException("Patient not found"));
+                .orElseThrow(() -> {
+                    log.error("Paciente com ID {} não encontrado.", appointmentRequestDto.getPatientId());
+                    return new IllegalArgumentException("Patient not found");
+                });
+
         Doctor doctor = this.doctorRepository
                 .findById(appointmentRequestDto.getDoctorId())
-                .orElseThrow(() -> new IllegalArgumentException("Doctor not found"));
+                .orElseThrow(() -> {
+                    log.error("Médico com ID {} não encontrado.", appointmentRequestDto.getDoctorId());
+                    return new IllegalArgumentException("Doctor not found");
+                });
 
         Appointment appointment = new Appointment();
         appointment.setDate(appointmentRequestDto.getDate());
@@ -36,24 +47,38 @@ public class AppointmentService {
 
         Appointment savedAppointment = this.appointmentRepository.save(appointment);
 
+        log.info("Agendamento criado com sucesso. ID do agendamento: {}", savedAppointment.getId());
+
         return AppointmentResponseDto.fromEntity(savedAppointment);
     }
 
+
     public Page<AppointmentResponseDto> findAll(Pageable pageable) {
+        log.info("Iniciando busca paginada de agendamentos. Página: {}, Tamanho: {}",
+                pageable.getPageNumber(), pageable.getPageSize());
         Page<Appointment> appointments = this.appointmentRepository.findAll(pageable);
 
+        log.info("Busca finalizada. Total de agendamentos encontrados: {}", appointments.getTotalElements());
         return appointments.map(AppointmentResponseDto::fromEntity);
     }
 
     public Page<AppointmentResponseDto> findAllAppointmentsByDoctorId(Pageable pageable, Long id) {
+        log.info("Iniciando busca paginada de agendamentos para o médico com ID: {}. Página: {}, Tamanho: {}",
+                id, pageable.getPageNumber(), pageable.getPageSize());
         Page<Appointment> appointments = this.appointmentRepository.findAllAppointmentsByDoctorId(pageable, id);
 
+        log.info("Busca finalizada. Total de agendamentos encontrados para o médico {}: {}",
+                id, appointments.getTotalElements());
         return appointments.map(AppointmentResponseDto::fromEntity);
     }
 
     public Page<AppointmentResponseDto> findAllAppointmentsByPatientId(Pageable pageable, Long id) {
+        log.info("Iniciando busca paginada de agendamentos para o paciente com ID: {}. Página: {}, Tamanho: {}",
+                id, pageable.getPageNumber(), pageable.getPageSize());
         Page<Appointment> appointments = this.appointmentRepository.findAllAppointmentsByPatientId(pageable, id);
 
+        log.info("Busca finalizada. Total de agendamentos encontrados para o paciente {}: {}",
+                id, appointments.getTotalElements());
         return appointments.map(AppointmentResponseDto::fromEntity);
     }
 }
