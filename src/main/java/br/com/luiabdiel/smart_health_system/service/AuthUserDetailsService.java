cd0a78905986ea1db3af54811aa2e7c2c086dde1
luiabdiel <luiabdieldev@gmail.com>
2025-05-07 08:39:36 -0300

    add logs in AuthUserDetailsService

--- src/main/java/br/com/luiabdiel/smart_health_system/service/AuthUserDetailsService.java
+++ src/main/java/br/com/luiabdiel/smart_health_system/service/AuthUserDetailsService.java
@@ -2,6 +2,7 @@ package br.com.luiabdiel.smart_health_system.service;
 
 import br.com.luiabdiel.smart_health_system.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,15 +10,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Carregando dados do usuário para autenticação: {}", username);
+
         br.com.luiabdiel.smart_health_system.model.User user = this.userRepository
                 .findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException("User not found."));
+                .orElseThrow(() -> {
+                    log.warn("Usuário não encontrado: {}", username);
+                    return new UsernameNotFoundException("User not found.");
+                });
+
+        log.info("Usuário encontrado: {}. Retornando dados para autenticação.", username);
 
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getUsername())
