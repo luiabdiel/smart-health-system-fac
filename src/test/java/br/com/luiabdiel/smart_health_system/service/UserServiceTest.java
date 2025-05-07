6592f3f89e295cd4dfb077dfb88a9ee4d7c2be1d
luiabdiel <luiabdieldev@gmail.com>
2025-05-07 08:11:07 -0300

    test: shouldRegisterUserSuccessfully

---
+++ src/test/java/br/com/luiabdiel/smart_health_system/service/UserServiceTest.java
@@ -0,0 +1,51 @@
+package br.com.luiabdiel.smart_health_system.service;
+
+import br.com.luiabdiel.smart_health_system.controller.dto.UserRequestDto;
+import br.com.luiabdiel.smart_health_system.controller.dto.UserResponseDto;
+import br.com.luiabdiel.smart_health_system.model.User;
+import br.com.luiabdiel.smart_health_system.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void shouldRegisterUserSuccessfully() {
+        Long expectedId = 1L;
+        String expectedUsername = "any_name";
+        String rawPassword = "any_password";
+        String encodedPassword = "any_password_encoded";
+
+        UserRequestDto userRequestDto = new UserRequestDto(expectedUsername, rawPassword);
+        User savedUser = new User(expectedId, expectedUsername, encodedPassword);
+
+        when(passwordEncoder.encode(rawPassword)).thenReturn(encodedPassword);
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        UserResponseDto response = userService.registerUser(userRequestDto);
+
+        assertNotNull(response);
+        assertEquals(expectedId, response.getId());
+        assertEquals(expectedUsername, response.getUsername());
+        verify(passwordEncoder, times(1)).encode(rawPassword);
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}
