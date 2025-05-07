4371c8639eeed3e3fa62476e2c67aca2128c0421
luiabdiel <luiabdieldev@gmail.com>
2025-05-07 08:38:20 -0300

    add logs in UserService

--- src/main/java/br/com/luiabdiel/smart_health_system/service/UserService.java
+++ src/main/java/br/com/luiabdiel/smart_health_system/service/UserService.java
@@ -6,38 +6,56 @@ import br.com.luiabdiel.smart_health_system.model.User;
 import br.com.luiabdiel.smart_health_system.repository.UserRepository;
 import br.com.luiabdiel.smart_health_system.shared.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     public UserResponseDto registerUser(UserRequestDto userRequestDto) {
+        log.info("Iniciando registro de novo usuário: {}", userRequestDto.getUsername());
         String encryptedPassword = this.passwordEncoder.encode(userRequestDto.getPassword());
         User user = new User(userRequestDto.getUsername(), encryptedPassword);
 
         var savedUser = this.userRepository.save(user);
+
+        log.info("Usuário registrado com sucesso. ID: {}", savedUser.getId());
         return UserResponseDto.fromEntity(savedUser);
     }
 
     public UserResponseDto findByUsername(String username) {
+        log.info("Iniciando busca de usuário com username: {}", username);
         User user = this.userRepository
                 .findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException("User not found."));
+                .orElseThrow(() -> {
+                    log.warn("Usuário com username '{}' não encontrado.", username);
+                    return new IllegalArgumentException("User not found.");
+                });
 
+        log.info("Usuário encontrado. ID: {}", user.getId());
         return UserResponseDto.fromEntity(user);
     }
 
     public String login(UserRequestDto userRequestDto) {
+        log.info("Tentando autenticar usuário: {}", userRequestDto.getUsername());
+
         User user = this.userRepository
                 .findByUsername(userRequestDto.getUsername())
                 .filter(u -> this.passwordEncoder.matches(userRequestDto.getPassword(), u.getPassword()))
-                .orElseThrow(() -> new IllegalArgumentException("Invalid credentials."));
+                .orElseThrow(() -> {
+                    log.warn("Falha na autenticação para o usuário: {}", userRequestDto.getUsername());
+                    return new IllegalArgumentException("Invalid credentials.");
+                });
 
-        return JwtUtil.generateToken(user.getUsername());
+        String token = JwtUtil.generateToken(user.getUsername());
+        log.info("Usuário autenticado com sucesso: {}. Token gerado.", user.getUsername());
+
+        return token;
     }
 }
