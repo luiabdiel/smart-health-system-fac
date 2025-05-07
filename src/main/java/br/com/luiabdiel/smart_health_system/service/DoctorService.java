72faabe6b4b0631007a3f44158d2c53e2c60875e
luiabdiel <luiabdieldev@gmail.com>
2025-05-07 08:41:19 -0300

    add logs in DoctorRepository

--- src/main/java/br/com/luiabdiel/smart_health_system/service/DoctorService.java
+++ src/main/java/br/com/luiabdiel/smart_health_system/service/DoctorService.java
@@ -5,36 +5,56 @@ import br.com.luiabdiel.smart_health_system.controller.dto.DoctorResponseDto;
 import br.com.luiabdiel.smart_health_system.model.Doctor;
 import br.com.luiabdiel.smart_health_system.repository.DoctorRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DoctorService {
 
     private final DoctorRepository doctorRepository;
 
     public DoctorResponseDto create(DoctorRequestDto doctorRequestDto) {
+        log.info("Iniciando o cadastro do médico com CRM: {}", doctorRequestDto.getCrm());
+
         this.doctorRepository
                 .findByCrm(doctorRequestDto.getCrm())
-                .ifPresent(doctor -> { throw new IllegalArgumentException("CRM already registered");});
+                .ifPresent(doctor -> {
+                    log.warn("CRM já registrado: {}", doctorRequestDto.getCrm());
+                    throw new IllegalArgumentException("CRM already registered");
+                });
 
         Doctor doctor = this.doctorRepository.save(doctorRequestDto.toEntity());
+
+        log.info("Médico cadastrado com sucesso. ID: {}", doctor.getId());
 
         return DoctorResponseDto.fromEntity(doctor);
     }
 
     public Page<DoctorResponseDto> findAll(Pageable pageable) {
-        Page<Doctor> patients = this.doctorRepository.findAll(pageable);
+        log.info("Iniciando busca paginada de médicos. Página: {}, Tamanho: {}", pageable.getPageNumber(), pageable.getPageSize());
 
-        return patients.map(DoctorResponseDto::fromEntity);
+        Page<Doctor> doctors = this.doctorRepository.findAll(pageable);
+
+        log.info("Busca finalizada. Total de médicos encontrados: {}", doctors.getTotalElements());
+
+        return doctors.map(DoctorResponseDto::fromEntity);
     }
 
     public DoctorResponseDto findByCrm(String crm) {
+        log.info("Iniciando busca pelo médico com CRM: {}", crm);
+
         Doctor doctor = this.doctorRepository
                 .findByCrm(crm)
-                .orElseThrow(() -> new IllegalArgumentException("Doctor not found"));
+                .orElseThrow(() -> {
+                    log.warn("Médico com CRM '{}' não encontrado.", crm);
+                    return new IllegalArgumentException("Doctor not found");
+                });
+
+        log.info("Médico encontrado com CRM: {}", crm);
 
         return DoctorResponseDto.fromEntity(doctor);
     }
